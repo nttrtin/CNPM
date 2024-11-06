d5a04cbb1162873d837c2bf72c618a0f3eecc1c6
GioSaDac <145772166+GioSaDac@users.noreply.github.com>
2024-11-06 09:11:50 +0700

    Add files via upload

---
+++ TonGiaoController.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using QLNS.Models;
+
+namespace QLNS.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class TonGiaoController : Controller
+    {
+        private readonly QuanlynhansuDbContext _context;
+
+        public TonGiaoController(QuanlynhansuDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TonGiao
+        public async Task<IActionResult> Index()
+        {
+            return _context.TonGiao != null ?
+                        View(await _context.TonGiao.ToListAsync()) :
+                        Problem("Entity set 'QuanlynhansuDbContext.TonGiao'  is null.");
+        }
+
+        // GET: TonGiao/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.TonGiao == null)
+            {
+                return NotFound();
+            }
+
+            var tonGiao = await _context.TonGiao
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (tonGiao == null)
+            {
+                return NotFound();
+            }
+
+            return View(tonGiao);
+        }
+
+        // GET: TonGiao/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TonGiao/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,MaTonGiao,TenTonGiao")] TonGiao tonGiao)
+        {
+            // Kiểm tra trùng mã trước khi thêm mới
+            if (!IsDuplicateCode(tonGiao.MaTonGiao))
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Add(tonGiao);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    return View(tonGiao);
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("MaTonGiao", "Mã Đã Có Trong Hệ Thống, Hãy Nhập Mã Mới!");
+                return View(tonGiao);
+            }
+        }
+        public bool IsDuplicateCode(string code)
+        {
+            return _context.TonGiao.Any(b => b.MaTonGiao == code);
+        }
+        // GET: TonGiao/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.TonGiao == null)
+            {
+                return NotFound();
+            }
+
+            var tonGiao = await _context.TonGiao.FindAsync(id);
+            if (tonGiao == null)
+            {
+                return NotFound();
+            }
+            return View(tonGiao);
+        }
+
+        // POST: TonGiao/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,MaTonGiao,TenTonGiao")] TonGiao tonGiao)
+        {
+            if (id != tonGiao.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tonGiao);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TonGiaoExists(tonGiao.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tonGiao);
+        }
+
+        // GET: TonGiao/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.TonGiao == null)
+            {
+                return NotFound();
+            }
+
+            var tonGiao = await _context.TonGiao
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (tonGiao == null)
+            {
+                return NotFound();
+            }
+
+            return View(tonGiao);
+        }
+
+        // POST: TonGiao/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            try
+            {
+                var tonGiao = await _context.TonGiao.FindAsync(id);
+                if (tonGiao == null)
+                {
+                    return NotFound("Không thể tìm thấy chức vụ để xóa.");
+                }
+
+                _context.TonGiao.Remove(tonGiao);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateException ex)
+            {
+                var innerException = ex.InnerException;
+                if (innerException is SqlException sqlEx && sqlEx.Number == 547) // Foreign key constraint violation
+                {
+                    TempData["ErrorMessage"] = "Không thể xóa chức vụ vì đã được liên kết với bảng nhân viên.";
+                    return RedirectToAction(nameof(Index)); // Redirect back to the index page
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = $"Đã xảy ra lỗi khi xóa chức vụ: {ex.Message}";
+                    return RedirectToAction(nameof(Index)); // Redirect back to the index page
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = $"Đã xảy ra lỗi khi xóa chức vụ: {ex.Message}";
+                return RedirectToAction(nameof(Index)); // Redirect back to the index page
+            }
+        }
+
+        private bool TonGiaoExists(int id)
+        {
+            return (_context.TonGiao?.Any(e => e.ID == id)).GetValueOrDefault();
+        }
+    }
+}
